b277d3b5ff2f55acaf98583483ccc5a7668940e4
Hayk Balasanyan <haykbalasanyan1@gmail.com>
2019-02-13 12:15:20 +0400

    change asset to secure

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -11,10 +11,10 @@
     <title>CasinoCode - Community</title>
     <link rel="alternate" href="https://www.casinocode.net/dk/community/login.php" hreflang="dk" /><link rel="alternate" href="https://www.casinocode.net/de/community/login.php" hreflang="de" /><link rel="alternate" href="https://www.casinocode.net/en/community/login.php" hreflang="en" /><link rel="alternate" href="https://www.casinocode.net/fi/community/login.php" hreflang="fi" /><link rel="alternate" href="https://www.casinocode.net/fr/community/login.php" hreflang="fr" /><link rel="alternate" href="https://www.casinocode.net/nl/community/login.php" hreflang="nl" /><link rel="alternate" href="https://www.casinocode.net/it/community/login.php" hreflang="it" /><link rel="alternate" href="https://www.casinocode.net/no/community/login.php" hreflang="no" /><link rel="alternate" href="https://www.casinocode.net/se/community/login.php" hreflang="se" />
     <!-- Bootstrap 4.0-->
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.css') }}">
 
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('css/master_style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/master_style.css') }}">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -37,13 +37,13 @@
 
 
 <!-- jQuery 3 -->
-<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ secure_asset('js/jquery.min.js') }}"></script>
 
 <!-- popper -->
-<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ secure_asset('js/popper.min.js') }}"></script>
 
 <!-- Bootstrap 4.0-->
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('js/bootstrap.min.js') }}"></script>
 
 </body>
 </html>
